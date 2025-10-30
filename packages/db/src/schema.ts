626994aab03024fb1c1acaf747b94d5507607070
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-10-31 00:54:12 +0530

    feat: Add car make/model/modification new schemas

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -3,7 +3,6 @@ export * from "./tables/car";
 export * from "./tables/chat";
 export * from "./tables/favourite";
 export * from "./tables/listing";
-export * from "./tables/post";
 export * from "./tables/review";
 export * from "./tables/waitlist";
 export * from "./tables/forum";
--- packages/db/src/tables/car.ts
+++ packages/db/src/tables/car.ts
@@ -3,18 +3,37 @@ import { pgTable } from "drizzle-orm/pg-core";
 export const make = pgTable("makes", (t) => ({
   id: t.serial().primaryKey(),
   name: t.text().notNull().unique(),
+  slug: t.text(),
 }));
 
 export const model = pgTable("models", (t) => ({
   id: t.serial().primaryKey(),
   makeId: t
-    .integer()
+    .integer("makeId")
     .notNull()
-    .references(() => make.id),
+    .references(() => make.id, { onDelete: "cascade" }),
   name: t.text().notNull(),
+  slug: t.text(),
+  dateStart: t.text("dateStart"),
+  dateEnd: t.text("dateEnd"),
+}));
+
+export const modification = pgTable("modifications", (t) => ({
+  id: t.serial().primaryKey(),
+  modelId: t
+    .integer("modelId")
+    .notNull()
+    .references(() => model.id, { onDelete: "cascade" }),
+  name: t.text().notNull(),
+  slug: t.text(),
+  dateStart: t.text("dateStart"),
+  dateEnd: t.text("dateEnd"),
+  engineLiters: t.doublePrecision("engineLiters"),
+  engineType: t.text("engineType"),
 }));
 
 export const category = pgTable("categories", (t) => ({
   id: t.serial().primaryKey(),
   name: t.text().notNull().unique(),
+  description: t.text().notNull(),
 }));
